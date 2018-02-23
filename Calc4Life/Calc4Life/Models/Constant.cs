606da36fa3104e5b7bb7d0b05b5370c5a25cba1c
Sergey Krivobokov <writesd@hotmail.com>
2018-02-23 20:17:25 +0100

    added CategoryId into Constant

--- Calc4Life/Calc4Life/Models/Constant.cs
+++ Calc4Life/Calc4Life/Models/Constant.cs
@@ -10,6 +10,7 @@ namespace Calc4Life.Models
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+        public int CategoryId { get; set; }
         public string Name { get; set; }
         public double Value { get; set; }
         public string Note { get; set; }
