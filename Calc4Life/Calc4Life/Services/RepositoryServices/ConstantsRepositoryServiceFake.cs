bc43e96ad0414d80c64d0739836d944929c6c41e
Sergey Krivobokov <writesd@hotmail.com>
2018-03-14 17:50:19 +0100

    no comments

--- Calc4Life/Calc4Life/Services/RepositoryServices/ConstantsRepositoryServiceFake.cs
+++ Calc4Life/Calc4Life/Services/RepositoryServices/ConstantsRepositoryServiceFake.cs
@@ -69,8 +69,8 @@ namespace Calc4Life.Services.RepositoryServices
             return await Task.Run(() => _constants = new List<Constant>
             {
                 new Constant(){Id=1, Name="Example of constant", Value=1234567.89,
-                    Note ="This is the constant example. You can edit or delete this one. Also you can add another constants in list", IsFavorite=false},
-                new Constant(){Id=2, Name="Сan of beer", Value=10.55, Note="Средняя цена одной банки пива Евросоюзе в 2017 году", IsFavorite=false},
+                    Note ="This is the constant example. You can edit or delete this one. Also you can add another constants in the list", IsFavorite=false},
+                new Constant(){Id=2, Name="Сan of beer", Value=10.55, Note="Средняя цена одной банки пива в Евросоюзе в 2017 году", IsFavorite=false},
                 new Constant(){Id=3, Name="Second Constant", Value=23456789 , Note="Заметка о Second Constant", IsFavorite=false},
                 new Constant(){Id=4, Name="Third Constant", Value=34567890, Note="", IsFavorite=false},
                 new Constant(){Id=5, Name="Forth Constant", Value=456789 , Note="Заметка о Forth Constant", IsFavorite=false},
