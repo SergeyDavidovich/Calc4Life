e669193c4f5270edd9d647371f7b5004ab2e1428
Bek <Bekvevo@gmail.com>
2018-04-19 22:51:26 +0200

    Added AboutChatMessage on About Page, FileHelper for iOS

---
+++ Calc4Life/Calc4Life/Models/AboutMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calc4Life.Models
+{
+    public class AboutMessage
+    {
+        public bool IsAnswer { get; set; }
+        public string Text { get; set; }
+        public Uri ImageAttachmentUri { get; set; }
+        public bool HasImageAttachment { get; set; }
+    }
+}
--- Calc4Life/Calc4Life/ViewModels/AboutPageViewModel.cs
+++ Calc4Life/Calc4Life/ViewModels/AboutPageViewModel.cs
@@ -4,7 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Prism.Navigation;
-
+using System.Collections.ObjectModel;
+using Calc4Life.Models;
 
 namespace Calc4Life.ViewModels
 {
@@ -16,12 +17,30 @@ namespace Calc4Life.ViewModels
         {
             _navigationService = navigationService;
             NavigateCommand = new DelegateCommand(NavigateExecute);
-        }
+            AboutMessages = new ObservableCollection<AboutMessage>();
+            AboutMessages.Add(new AboutMessage() { IsAnswer = false, Text = "Что такое константа?" });
+            AboutMessages.Add(new AboutMessage() { IsAnswer = true, Text = "Константы это постоянные значения в виде цены на товар или услугу. Константы позволяют хранить все значения прямо в калькуляторе" });
+            AboutMessages.Add(new AboutMessage() { IsAnswer = false, Text = "Как добавить константу?" });
+            AboutMessages.Add(new AboutMessage() { IsAnswer = true, Text = "Для этого нужно нажать ввести значения и нажать на кнопку +, дать имя константе и сохранить" });
+            AboutMessages.Add(new AboutMessage() { IsAnswer = false, Text = "Как использовать константы при счёте?" });
+            AboutMessages.Add(new AboutMessage() { IsAnswer = false, Text = "Нажмите на кнопку CONST, выберите константу и нажмите на зеленую кнопку" });
 
+        }
+        public override void OnNavigatedTo(NavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+            
+        }
         public DelegateCommand NavigateCommand { get; }
         private void NavigateExecute()
         {
             Xamarin.Forms.Device.OpenUri(new Uri("mailto:writesd@hotmail.com?subject=Calc4Life%20Feedback"));
+        }
+        private ObservableCollection<AboutMessage> _aboutMessages;
+        public ObservableCollection<AboutMessage> AboutMessages
+        {
+            get => _aboutMessages;
+            set => SetProperty(ref _aboutMessages, value);
         }
     }
 }
