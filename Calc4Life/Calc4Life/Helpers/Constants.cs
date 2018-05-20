42d52b9ec76cbb7b0e21fa35ad056c4be3eb465a
Sergey Krivobokov <writesd@hotmail.com>
2018-05-20 09:10:28 +0200

    changed settings added app constants

--- Calc4Life/Calc4Life/Helpers/Constants.cs
+++ Calc4Life/Calc4Life/Helpers/Constants.cs
@@ -9,11 +9,9 @@ namespace Calc4Life.Helpers
         //in app messages
         public const string SETTINGS_CHANGED_MESSAGE = "settings_changed";
 
-        //Application.Properties keys
-        public const string KEY_CONSTANTS_NUMBER = "constants_number"; // текущее количество констант
-        public const string KEY_MAX_CONSTANTS_NUMBER = "max_constants_number"; // количество констант, доступных без покупки
+        //constant purchasing 
+        public const int MAX_CONSTANTS_NUMBER = 3; // количество констант, доступных без покупки
         public const string CONSTANTS_PPODUCT_ID = "constants_unblocked"; // имя (id) продукта для продажи (зарегистрировано в Google Play)
-        public const string IS_CONSTANT_PURCHASED = "is_constant_purchased"; // приобретен ли продукт "constants_unblocked"
 
         //common app constants
 
